2bf4ffd1c251e7c3130a59da3086488c013c792f
Vikas Rathore <55776424+VikasRathore162@users.noreply.github.com>
2020-09-19 12:44:10 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,44 @@
+
+<header>
+		<div class="top-bar_sub_w3layouts container-fluid">
+			<div class="row">
+				<div class="col-md-4 logo text-left">
+					<a class="navbar-brand" href="index.php">
+						<i class="fab fa-linode"></i> WeJAPA Internship Blog</a>
+				</div>
+				<div class="col-md-4 top-forms text-center mt-lg-3 mt-md-1 mt-0">
+					<span>Welcome Back!</span>
+					<span class="mx-lg-4 mx-md-2  mx-1">
+						<a href="blogadmin/index.php">
+							<i class="fas fa-lock"></i> <strong>Sign In</strong></a>
+					</span>
+					<span>
+						<a href="blogadmin/membership_signup.php">
+							<i class="far fa-user"></i> <strong>Register</strong></a>
+					</span>
+				</div>
+
+			</div>
+		</div>
+
+			<div class="header_top" id="home">
+				<nav class="navbar navbar-expand-lg navbar-light bg-light">
+					<button class="navbar-toggler navbar-toggler-right mx-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+						aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+				   </button>
+
+
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul class="navbar-nav mr-auto">
+							<li class="nav-item active">
+								<a class="nav-link" href="index.php">Home
+									<span class="sr-only">(current)</span>
+								</a>
+							</li>
+
+					</div>
+				</nav>
+
+			</div>
+	</header>
